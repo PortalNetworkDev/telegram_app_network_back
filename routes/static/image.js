e28c0d108428efd77d6c507a321af2ab2680d9e7
Mikhail <spiritkhl@gmail.com>
2024-04-25 21:43:45 +0300

    fix params in static images and lang

--- routes/static/image.js
+++ routes/static/image.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/images/:image', function (req, reply) {
-        reply.sendFile(`/images/${req.params.icon}`) 
+        reply.sendFile(`/images/${req.params.image}`) 
     })  
 }
--- routes/static/lang.js
+++ routes/static/lang.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/lang/:lang', function (req, reply) {
-        reply.sendFile(`/lang/${req.params.html}`) 
+        reply.sendFile(`/lang/${req.params.lang}`) 
     })  
 }
