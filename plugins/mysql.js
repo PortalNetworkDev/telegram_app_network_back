9470c7ba6abce243eef4997642392a115430fb23
Mikhail <spiritkhl@gmail.com>
2024-04-26 02:10:08 +0300

    try to fix mysql freeze

--- plugins/mysql.js
+++ plugins/mysql.js
@@ -14,9 +14,13 @@ module.exports = fp(async function (f, opts) {
 
     f.mysql.select = async function(sql,values) {
         try {     
+            console.log("select before init connection")
             const connection = await f.mysql.getConnection()     
+            console.log("select after init connection")
             const [rows, fields] = await connection.execute(sql, values);
+            console.log("select after execute")
             connection.release()
+            console.log("select after release")
             return {rows,fields}
           } catch (err) {
             console.log(err);
@@ -25,9 +29,13 @@ module.exports = fp(async function (f, opts) {
 
     f.mysql.insert = async function(sql,values) {
         try {     
+            console.log("insert|update before init connection")
             const connection = await f.mysql.getConnection()     
+            console.log("insert|update after init connection")  
             const [result, fields] = await connection.execute(sql, values);
+            console.log("insert|update after execute")
             connection.release()
+            console.log("insert|update after release")
             return {result,fields}
           } catch (err) {
             console.log(err);
