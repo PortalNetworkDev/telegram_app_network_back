6399db4dc239a29cedb123f619f79b01f54adc59
Mikhail <spiritkhl@gmail.com>
2024-05-02 19:26:52 +0300

    refactor createpayload

--- plugins/auth.js
+++ plugins/auth.js
@@ -20,6 +20,7 @@ module.exports = fp(async function (fastify, opts) {
         }
 
 
+
         if(!verifyTelegramWebAppData(fastify.config.bottoken,req.headers.authorization)){
             console.log("Access denied with token:", req.headers.authorization)
             return res.unauthorized("Access denied");
@@ -52,46 +53,26 @@ module.exports = fp(async function (fastify, opts) {
     })
 })
 
-function createpayload(auth){
-    const initData = new URLSearchParams(auth);
-
-
-    let payload = `auth_date=${initData.get("auth_date")}\nchat_instance=${initData.get("chat_instance")}\nchat_type=${initData.get("chat_type")}\n`;
+function createpayload(initData){
     
-    if(initData.get("start_param")){
-        payload = payload+`start_param=${initData.get("start_param")}\n`
+    let array = []
+    
+    initData.sort()
+
+    for (const [key, value] of initData.entries()) {
+        if(key != "hash")
+            array.push(`${key}=${value}`)
     }
 
-
-    let user = "";
-
-
-
-    let _split = auth.split("&")
-
-    for (let index = 0; index < _split.length; index++) {
-        const element = _split[index];
-        
-        let _elem = element.split("=")
-
-        if(_elem[0] == "user"){
-            user = decodeURI(_elem[1]).replaceAll("%3A", ":").replaceAll("%2C",",");
-        }
-    }
-    
-    return payload+"user="+user;
+    return array.join("\n")
 }
 
 function verifyTelegramWebAppData(TELEGRAM_BOT_TOKEN,telegramInitData){
     const initData = new URLSearchParams(telegramInitData);
-    const payload = createpayload(telegramInitData)
+    const payload = createpayload(initData)
     const hash = initData.get("hash");
-
     const secret_key = CryptoJS.HmacSHA256(TELEGRAM_BOT_TOKEN, "WebAppData");
-    //console.log("secret_key", secret_key.toString())
     const calculated_hash = CryptoJS.HmacSHA256(payload, secret_key).toString();
-    //console.log("calculated_hash", calculated_hash)
-    //console.log("hash",hash)
     return calculated_hash === hash;
 }
 
