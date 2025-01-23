7c4547f94c2f75beaea8463cd26a5f1e79c19b61
Roamer <jatgaret@gmail.com>
2025-01-23 14:22:34 +0300

    add hasNextPage

--- models/transactions/transactions.types.ts
+++ models/transactions/transactions.types.ts
@@ -39,4 +39,6 @@ export interface TransactionsService {
     userId: number,
     amount: number
   ) => Promise<Transaction[]>;
+
+  getTransactionsTableLength: (userId: number) => Promise<number>;
 }
